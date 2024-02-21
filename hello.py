73212bcc62cb18a865cd5d654a3ee3aa12e0ffce
Bruno Sousa <brunodsousa@outlook.com>
2024-02-21 09:37:41 -0300

    Adaptando idioma da mensagem para linguagem configurada no SO

--- hello.py
+++ hello.py
@@ -1,3 +1,33 @@
 #!/usr/bin/env python3
-# Imprime "Hello, World!"
-print('Hello, World!')
+"""Hello World Multilingual
+
+Depending on the language configured in the environment, the program displays
+the corresponding message.
+
+Usage:
+
+Have a properly configured LANG variable. Example:
+
+    export LANG=pt_BR
+
+Execution:
+
+    python3 hello.py
+    or
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Bruno Sousa"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+message = "Hello, World!"
+
+if current_language == "pt_BR":
+    message = "Olá, Mundo!"
+elif current_language == "it_IT": 
+    message = "Ciao, Mondo!"
+    
+print(message)
